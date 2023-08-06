ae9a089bf498183f44dbf6bd117278fd3d7b43c9
Earthyy <elardi2001@gmail.com>
2023-08-06 20:34:47 +0100

    fix: fix svg flashing issue on reload

--- src/components/custom/PlayButton.tsx
+++ src/components/custom/PlayButton.tsx
@@ -7,6 +7,8 @@ const PlayButton: FC<PlayButtonProps> = (props) => {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 75 75"
+      width={75}
+      height={75}
       className="cursor-pointer w-12 h-12 md:w-[4.6875rem] md:h-[4.6875rem] group"
       {...props}
     >
@@ -15,7 +17,8 @@ const PlayButton: FC<PlayButtonProps> = (props) => {
           cx="37.5"
           cy="37.5"
           r="37.5"
-          className="opacity-25 group-hover:opacity-100"
+          opacity={0.25}
+          className="group-hover:opacity-100"
         />
         <path d="M29 27v21l21-10.5z" className="group-hover:fill-white" />
       </g>
