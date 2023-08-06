bb0e7dac12350d820d60a768b222efcfb6084b56
Earthyy <elardi2001@gmail.com>
2023-08-06 19:13:14 +0100

    chore: extend screen vars to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      screens: {
+        mobile: "375px",
+        tablet: "768px",
+        desktop: "1440px",
+      },
       colors: {
         primary: "var(--text-color-primary)",
         secondary: "var(--text-color-secondary)",
