0a054279cde254e5e529ae40a907a7b91c9faacb
Earthyy <elardi2001@gmail.com>
2023-08-06 18:48:52 +0100

    chore: set up font & color variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <></>;
+  return (
+    <div className="light lora">
+      <h1 className="heading-lg">Hello, World</h1>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--text-color-primary)",
+        secondary: "var(--text-color-secondary)",
+        tertiary: "var(--text-color-tertiary)",
+        inputColor: "var(--input-bg-color)",
+        toggleColor: "var(--toggle-bg-color)",
+      },
+    },
   },
   plugins: [],
 };
