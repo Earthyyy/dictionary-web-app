eea85b8092902bf79e0dcd7dfd4aaa95e8a9b2d6
Earthyy <elardi2001@gmail.com>
2023-08-06 19:42:33 +0100

    feat: build PlayButton Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import PlayButton from "./components/custom/PlayButton";
+
 function App() {
   return (
     <div className="light lora">
       <h1 className="heading-lg">Hello, World</h1>
+      <PlayButton />
     </div>
   );
 }
---
+++ src/components/custom/PlayButton.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+
+interface PlayButtonProps {}
+
+const PlayButton: FC<PlayButtonProps> = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 75 75"
+      className="cursor-pointer w-12 h-12 md:w-[4.6875rem] md:h-[4.6875rem] group"
+      {...props}
+    >
+      <g className="fill-primary" fill-rule="evenodd">
+        <circle
+          cx="37.5"
+          cy="37.5"
+          r="37.5"
+          className="opacity-25 group-hover:opacity-100"
+        />
+        <path d="M29 27v21l21-10.5z" className="group-hover:fill-white" />
+      </g>
+    </svg>
+  );
+};
+
+export default PlayButton;
