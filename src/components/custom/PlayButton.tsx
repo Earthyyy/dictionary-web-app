4a908cc752aceebf3bb71a964bb0a8322e100357
Earthyy <elardi2001@gmail.com>
2023-08-07 15:36:22 +0100

    fix: fix screen breakpoint typo

--- src/components/custom/PlayButton.tsx
+++ src/components/custom/PlayButton.tsx
@@ -9,7 +9,7 @@ const PlayButton: FC<PlayButtonProps> = (props) => {
       viewBox="0 0 75 75"
       width={75}
       height={75}
-      className="cursor-pointer w-12 h-12 md:w-[4.6875rem] md:h-[4.6875rem] group"
+      className="cursor-pointer w-12 h-12 tablet:w-[4.6875rem] tablet:h-[4.6875rem] group"
       {...props}
     >
       <g className="fill-primary" fillRule="evenodd">
