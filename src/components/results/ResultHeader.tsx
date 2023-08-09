2c4484bc0ec1a20a7d4343a1d95a21c23e6f1e36
Earthyy <elardi2001@gmail.com>
2023-08-09 15:20:30 +0100

    feat: capitalize the heading title

--- src/components/results/ResultHeader.tsx
+++ src/components/results/ResultHeader.tsx
@@ -23,7 +23,9 @@ const ResultHeader: FC<Props> = ({ header }) => {
       {header && (
         <header className="flex items-center justify-between mb-7 tablet:mb-10">
           <div className="flex flex-col tablet:gap-y-2">
-            <h1 className="text-secondary heading-lg">{header.word}</h1>
+            <h1 className="text-secondary heading-lg">
+              {header.word.charAt(0).toUpperCase() + header.word.slice(1)}
+            </h1>
             <span className="text-primary body-md tablet:heading-md">
               {phonetic?.text}
             </span>
