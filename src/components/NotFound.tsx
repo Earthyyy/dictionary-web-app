c19d3855f09f6415dd258860f721396cd5e3750a
Earthyy <elardi2001@gmail.com>
2023-08-09 15:40:08 +0100

    fix: fix heading color in dark mode

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -2,7 +2,9 @@ const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center text-center gap-6 pt-16">
       <span className="heading-lg">😕</span>
-      <h3 className="heading-sm font-bold">No Definitions Found</h3>
+      <h3 className="heading-sm font-bold text-secondary">
+        No Definitions Found
+      </h3>
       <p className="body-md text-tertiary">
         Sorry pal, we couldn't find definitions for the word you were looking
         for. You can try the search again at later time or head to the web
